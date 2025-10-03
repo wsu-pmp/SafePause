c69e556a34b59518d8dfbcc8808fd9bf41c33ac5
jd-u <70433802+jd-u@users.noreply.github.com>
2025-10-03 12:58:05 +1000

    Add dsr_zed_moveit_config package

---
+++ src/dsr_zed_moveit_config/launch/setup_assistant.launch.py
@@ -0,0 +1,9 @@
+from moveit_configs_utils import MoveItConfigsBuilder
+from moveit_configs_utils.launches import generate_setup_assistant_launch
+
+
+def generate_launch_description():
+    moveit_config = MoveItConfigsBuilder(
+        "a0509", package_name="dsr_zed_moveit_config"
+    ).to_moveit_configs()
+    return generate_setup_assistant_launch(moveit_config)
---
+++ src/dsr_zed_moveit_config/launch/static_virtual_joint_tfs.launch.py
@@ -0,0 +1,9 @@
+from moveit_configs_utils import MoveItConfigsBuilder
+from moveit_configs_utils.launches import generate_static_virtual_joint_tfs_launch
+
+
+def generate_launch_description():
+    moveit_config = MoveItConfigsBuilder(
+        "a0509", package_name="dsr_zed_moveit_config"
+    ).to_moveit_configs()
+    return generate_static_virtual_joint_tfs_launch(moveit_config)
