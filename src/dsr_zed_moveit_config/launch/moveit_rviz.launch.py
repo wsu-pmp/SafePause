cd55ce6d19ae6e14dbe80f256f4ba245b9ec3942
jd-u <70433802+jd-u@users.noreply.github.com>
2025-10-03 12:58:05 +1000

    Add dsr_zed_moveit_config package

---
+++ src/dsr_zed_moveit_config/launch/moveit_rviz.launch.py
@@ -0,0 +1,9 @@
+from moveit_configs_utils import MoveItConfigsBuilder
+from moveit_configs_utils.launches import generate_moveit_rviz_launch
+
+
+def generate_launch_description():
+    moveit_config = MoveItConfigsBuilder(
+        "a0509", package_name="dsr_zed_moveit_config"
+    ).to_moveit_configs()
+    return generate_moveit_rviz_launch(moveit_config)
