79c2ea6ea78bf688daf9c79a5624ee3478aa41a7
jd-u <70433802+jd-u@users.noreply.github.com>
2025-10-03 17:22:08 +1000

    Add perception_node launch args to bringup

--- src/bringup/launch/bringup.launch.py
+++ src/bringup/launch/bringup.launch.py
@@ -1,17 +1,43 @@
 from launch import LaunchDescription
-from launch.actions import OpaqueFunction
+from launch.actions import DeclareLaunchArgument, OpaqueFunction, Shutdown
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
-    declared_arguments = []
+    declared_arguments = [
+        DeclareLaunchArgument(
+            "config_file",
+            default_value=PathJoinSubstitution(
+                [FindPackageShare("perception_pkg"), "config", "example.yaml"]
+            ),
+            description="Topic config for perception_node, which requires one",
+        ),
+    ]
 
     def launch_setup(context, *args, **kwargs):
         launches = []
 
-        launches.append(Node(package="perception_pkg", executable="perception_node"))
+        launches.append(
+            Node(
+                package="perception_pkg",
+                executable="perception_node",
+                output="screen",
+                parameters=[
+                    {"config_file": LaunchConfiguration("config_file")},
+                ],
+                on_exit=Shutdown(),
+            )
+        )
 
-        launches.append(Node(package="interrupt_ctrl", executable="interrupt_node"))
+        launches.append(
+            Node(
+                package="interrupt_ctrl",
+                executable="interrupt_node",
+                output="screen",
+            )
+        )
 
         return launches
 
