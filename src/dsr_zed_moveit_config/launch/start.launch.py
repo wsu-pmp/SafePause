43f3c041ce7ac7de75685e8367af282de10f323b
jd-u <70433802+jd-u@users.noreply.github.com>
2025-12-05 13:22:14 +1100

    Revert "Fix dsr_zed_moveit_config start.launch.py"
    This reverts commit dff3348f85c7d17f9ca17788ceb1e640ebe6a94f.

--- src/dsr_zed_moveit_config/launch/start.launch.py
+++ src/dsr_zed_moveit_config/launch/start.launch.py
@@ -1,10 +1,7 @@
 # Modified from
 # https://raw.githubusercontent.com/DoosanRobotics/doosan-robot2/refs/heads/humble/dsr_moveit2/dsr_moveit_config_a0509/launch/start.launch.py
 
-import os
 
-from ament_index_python.packages import get_package_share_directory
-from dsr_bringup2.utils import read_update_rate
 from launch import LaunchDescription
 from launch.actions import (
     DeclareLaunchArgument,
@@ -26,67 +23,39 @@ from launch_ros.substitutions import FindPackageShare
 
 # Moveit2
 from moveit_configs_utils import MoveItConfigsBuilder
+from moveit_configs_utils.launches import generate_move_group_launch
 
 
 def rviz_node_function(context):
-    """Evaluate the model value at launch time, find the package path, and then execute the launch file"""
-    # CHANGE: substitue package name and remove dynamic model_value
-    # model_value = LaunchConfiguration('model').perform(context)
-
-    # model_value_str = f"{model_value}"
-    package_name_str = "dsr_zed_moveit_config"
-
-    # Get the package path using FindPackageShare
-    package_path_str = FindPackageShare(package_name_str).perform(context)
-
-    print("Package name:", package_name_str)
-    print("Package path:", package_path_str)
-
-    # CHANGE: use default dsr_zed_moveit_config MoveItConfigsBuilder
+    # Moveit2 config replaced with dsr_zed config
     moveit_config = MoveItConfigsBuilder(
-        "a0509", package_name=package_name_str
+        "a0509", package_name="dsr_zed_moveit_config"
     ).to_moveit_configs()
+    move_group_launch = generate_move_group_launch(moveit_config)
 
-    run_move_group_node = Node(
-        package="moveit_ros_move_group",
-        executable="move_group",
-        # namespace=LaunchConfiguration('name'),
-        output="screen",
-        parameters=[
-            moveit_config.to_dict(),
-        ],
+    # RViz launchreplaced with dsr_zed moveit_rviz launch
+    rviz_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [
+                PathJoinSubstitution(
+                    [
+                        FindPackageShare("dsr_zed_moveit_config"),
+                        "launch",
+                        "moveit_rviz.launch.py",
+                    ]
+                )
+            ]
+        ),
+        condition=IfCondition(LaunchConfiguration("launch_rviz")),
     )
 
-    # RViz
-    # CHANGE: rviz config share dir launch -> rviz
-    rviz_base = os.path.join(get_package_share_directory(package_name_str), "rviz")
-    rviz_full_config = os.path.join(rviz_base, "moveit.rviz")
-
     return [
-        run_move_group_node,
-        Node(
-            package="rviz2",
-            executable="rviz2",
-            name="rviz2",
-            # namespace=LaunchConfiguration('name'),
-            output="log",
-            arguments=["-d", rviz_full_config],
-            parameters=[
-                moveit_config.robot_description,
-                moveit_config.robot_description_semantic,
-                moveit_config.planning_pipelines,
-                moveit_config.robot_description_kinematics,
-                moveit_config.joint_limits,
-            ],
-        ),
+        *move_group_launch.entities,
+        rviz_launch,
     ]
 
 
 def generate_launch_description():
-    # CHANGE: remove model launch arg
-    #         remove color launch arg
-    #         add controller_log_level launch arg
-    #         add launch_zed launch arg
     ARGUMENTS = [
         DeclareLaunchArgument("name", default_value="", description="NAME_SPACE"),
         DeclareLaunchArgument(
@@ -96,19 +65,11 @@ def generate_launch_description():
         DeclareLaunchArgument(
             "mode", default_value="virtual", description="OPERATION MODE"
         ),
-        # DeclareLaunchArgument('model', default_value = 'm0617',     description = 'ROBOT_MODEL'    ),
-        # DeclareLaunchArgument('color', default_value = 'white',     description = 'ROBOT_COLOR'    ),
-        DeclareLaunchArgument("gui", default_value="false", description="Start RViz2"),
-        DeclareLaunchArgument(
-            "gz", default_value="false", description="USE GAZEBO SIM"
-        ),
         DeclareLaunchArgument(
             "rt_host", default_value="192.168.137.50", description="ROBOT_RT_IP"
         ),
         DeclareLaunchArgument(
-            "controller_log_level",
-            default_value="warn",
-            description="ros2_control_node log-level",
+            "launch_rviz", default_value="true", description="Launch RViz?"
         ),
         DeclareLaunchArgument(
             "launch_zed",
@@ -117,172 +78,141 @@ def generate_launch_description():
         ),
     ]
 
-    update_rate = str(read_update_rate())  # get update_rate from yaml
+    def launch_setup(context, *args, **kwargs):
+        # Robot description replaced with custom a0509_description
+        robot_description_content = Command(
+            [
+                PathJoinSubstitution([FindExecutable(name="xacro")]),
+                " ",
+                PathJoinSubstitution(
+                    [
+                        FindPackageShare("a0509_description"),
+                        "urdf",
+                        "a0509.urdf.xacro",
+                    ]
+                ),
+                " name:=",
+                LaunchConfiguration("name"),
+                " host:=",
+                LaunchConfiguration("host"),
+                " rt_host:=",
+                LaunchConfiguration("rt_host"),
+                " port:=",
+                LaunchConfiguration("port"),
+                " mode:=",
+                LaunchConfiguration("mode"),
+            ]
+        )
 
-    # CHANGE: substitute a0509_description in control_node
-    robot_description_path = PathJoinSubstitution(
-        [
-            FindPackageShare("a0509_description"),
-            "urdf",
-            "a0509.urdf.xacro",
-        ]
-    )
-    robot_description_content = Command(
-        [
-            PathJoinSubstitution([FindExecutable(name="xacro")]),
-            " ",
-            robot_description_path,
-            " name:=",
-            LaunchConfiguration("name"),
-            " host:=",
-            LaunchConfiguration("host"),
-            " rt_host:=",
-            LaunchConfiguration("rt_host"),
-            " port:=",
-            LaunchConfiguration("port"),
-            " mode:=",
-            LaunchConfiguration("mode"),
-            " update_rate:=",
-            update_rate,
-        ]
-    )
+        robot_description = {"robot_description": robot_description_content}
 
-    robot_description = {"robot_description": robot_description_content}
-
-    robot_controllers = [
-        PathJoinSubstitution(
+        robot_controllers = PathJoinSubstitution(
             [
                 FindPackageShare("dsr_controller2"),
                 "config",
                 "dsr_controller2.yaml",
             ]
         )
-    ]
 
-    # CHANGE: hardcode model a0509
-    run_emulator_node = Node(
-        package="dsr_bringup2",
-        executable="run_emulator",
-        namespace=LaunchConfiguration("name"),
-        parameters=[
-            {"name": LaunchConfiguration("name")},
-            {"rate": 100},
-            {"standby": 5000},
-            {"command": True},
-            {"host": LaunchConfiguration("host")},
-            {"port": LaunchConfiguration("port")},
-            {"mode": LaunchConfiguration("mode")},
-            {"model": "a0509"},
-            {"gripper": "none"},
-            {"mobile": "none"},
-            {"rt_host": LaunchConfiguration("rt_host")},
-            # parameters_file_path       # If a parameter is set in both the launch file and a YAML file, the value from the YAML file will be used.
-        ],
-        output="screen",
-    )
-
-    # CHANGE: add controller_log_level arg to control_node
-    control_node = Node(
-        package="controller_manager",
-        executable="ros2_control_node",
-        namespace=LaunchConfiguration("name"),
-        parameters=[robot_description, robot_controllers],
-        output="both",
-        arguments=[
-            "--ros-args",
-            "--log-level",
-            LaunchConfiguration("controller_log_level"),
-        ],
-    )
-
-    # CHANGE: substitute a0509_description in robot_state_pub_node
-    robot_state_pub_node = Node(
-        package="robot_state_publisher",
-        executable="robot_state_publisher",
-        name="robot_state_publisher",
-        namespace=LaunchConfiguration("name"),
-        output="both",
-        parameters=[robot_description],
-    )
-
-    joint_state_broadcaster_spawner = Node(
-        package="controller_manager",
-        namespace=LaunchConfiguration("name"),
-        executable="spawner",
-        arguments=["joint_state_broadcaster", "-c", "controller_manager"],
-    )
-
-    robot_controller_spawner = Node(
-        package="controller_manager",
-        namespace=LaunchConfiguration("name"),
-        executable="spawner",
-        arguments=["dsr_controller2", "-c", "controller_manager"],
-    )
-
-    dsr_moveit_controller_spawner = Node(
-        package="controller_manager",
-        executable="spawner",
-        namespace=LaunchConfiguration("name"),
-        arguments=[
-            "dsr_moveit_controller",
-            "-c",
-            "controller_manager",
-        ],
-    )
-
-    # # Moveit2 config
-    rviz_node = OpaqueFunction(function=rviz_node_function)
-
-    # CHANGE: zed_wrapper
-    zed_wrapper = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            [
-                PathJoinSubstitution(
-                    [
-                        FindPackageShare("zed_wrapper"),
-                        "launch",
-                        "zed_camera.launch.py",
-                    ]
-                )
-            ]
-        ),
-        launch_arguments={"publish_tf": "false", "camera_model": "zed2i"}.items(),
-        condition=IfCondition(LaunchConfiguration("launch_zed")),
-    )
-
-    # joint_trajectory_controller_spawner = Node(
-    #     package="controller_manager",
-    #     # namespace=LaunchConfiguration('name'),
-    #     executable="spawner",
-    #     arguments=["dsr_joint_trajectory", "-c", "dsr/controller_manager", "-n", "dsr"],
-    # )
-
-    # Delay rviz start after `joint_state_broadcaster`
-    delay_rviz_after_joint_state_broadcaster_spawner = RegisterEventHandler(
-        event_handler=OnProcessExit(
-            target_action=robot_controller_spawner,
-            on_exit=[rviz_node],
+        run_emulator_node = Node(
+            package="dsr_bringup2",
+            executable="run_emulator",
+            namespace=LaunchConfiguration("name"),
+            parameters=[
+                {"name": LaunchConfiguration("name")},
+                {"rate": 100},
+                {"standby": 5000},
+                {"command": True},
+                {"host": LaunchConfiguration("host")},
+                {"port": LaunchConfiguration("port")},
+                {"mode": LaunchConfiguration("mode")},
+                {"model": "a0509"},
+                {"gripper": "none"},
+                {"mobile": "none"},
+                {"rt_host": LaunchConfiguration("rt_host")},
+            ],
+            output="screen",
         )
-    )
 
-    # # Delay start of robot_controller after `joint_state_broadcaster`
-    # delay_robot_controller_spawner_after_joint_state_broadcaster_spawner = RegisterEventHandler(
-    #     event_handler=OnProcessExit(
-    #         target_action=joint_state_broadcaster_spawner,
-    #         on_exit=[robot_controller_spawner],
-    #     )
-    # )
+        control_node = Node(
+            package="controller_manager",
+            executable="ros2_control_node",
+            namespace=LaunchConfiguration("name"),
+            parameters=[robot_description, robot_controllers],
+            output="both",
+        )
 
-    # CHANGE: add zed_wrapper
-    nodes = [
-        run_emulator_node,
-        robot_state_pub_node,
-        robot_controller_spawner,
-        delay_rviz_after_joint_state_broadcaster_spawner,
-        joint_state_broadcaster_spawner,
-        dsr_moveit_controller_spawner,
-        control_node,
-        zed_wrapper,
-    ]
+        robot_state_pub_node = Node(
+            package="robot_state_publisher",
+            executable="robot_state_publisher",
+            name="robot_state_publisher",
+            namespace=LaunchConfiguration("name"),
+            output="both",
+            parameters=[robot_description],  # replaced robot description
+        )
 
-    return LaunchDescription(ARGUMENTS + nodes)
+        joint_state_broadcaster_spawner = Node(
+            package="controller_manager",
+            namespace=LaunchConfiguration("name"),
+            executable="spawner",
+            arguments=["joint_state_broadcaster", "-c", "controller_manager"],
+        )
+
+        robot_controller_spawner = Node(
+            package="controller_manager",
+            namespace=LaunchConfiguration("name"),
+            executable="spawner",
+            arguments=["dsr_controller2", "-c", "controller_manager"],
+        )
+
+        dsr_moveit_controller_spawner = Node(
+            package="controller_manager",
+            executable="spawner",
+            namespace=LaunchConfiguration("name"),
+            arguments=[
+                "dsr_moveit_controller",
+                "-c",
+                "controller_manager",
+            ],
+        )
+
+        # Moveit2 config
+        rviz_node = OpaqueFunction(function=rviz_node_function)
+
+        # zed_wrapper
+        zed_wrapper = IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                [
+                    PathJoinSubstitution(
+                        [
+                            FindPackageShare("zed_wrapper"),
+                            "launch",
+                            "zed_camera.launch.py",
+                        ]
+                    )
+                ]
+            ),
+            launch_arguments={"publish_tf": "false", "camera_model": "zed2i"}.items(),
+            condition=IfCondition(LaunchConfiguration("launch_zed")),
+        )
+
+        # Delay rviz start after `joint_state_broadcaster`
+        delay_rviz_after_joint_state_broadcaster_spawner = RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=robot_controller_spawner,
+                on_exit=[rviz_node],
+            )
+        )
+
+        return [
+            run_emulator_node,
+            robot_state_pub_node,
+            robot_controller_spawner,
+            delay_rviz_after_joint_state_broadcaster_spawner,
+            joint_state_broadcaster_spawner,
+            dsr_moveit_controller_spawner,
+            control_node,
+            zed_wrapper,
+        ]
+
+    return LaunchDescription(ARGUMENTS + [OpaqueFunction(function=launch_setup)])
