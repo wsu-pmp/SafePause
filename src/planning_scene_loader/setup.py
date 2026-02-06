d668aa990f9a6986569aed7539a664b849a8f5bf
jd-u <70433802+jd-u@users.noreply.github.com>
2026-02-06 02:20:43 +0000

    Add planning_scene_loader package

---
+++ src/planning_scene_loader/setup.py
@@ -0,0 +1,28 @@
+from glob import glob
+
+from setuptools import find_packages, setup
+
+package_name = "planning_scene_loader"
+
+setup(
+    name=package_name,
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
+    data_files=[
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        ("share/" + package_name + "/config", glob("config/*.yaml")),
+    ],
+    install_requires=["setuptools"],
+    zip_safe=True,
+    maintainer="dev",
+    maintainer_email="dev@todo.todo",
+    description="TODO: Package description",
+    license="TODO: License declaration",
+    tests_require=["pytest"],
+    entry_points={
+        "console_scripts": [
+            "planning_scene_loader_node = planning_scene_loader.planning_scene_loader_node:main",
+        ],
+    },
+)
